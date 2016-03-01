901f545107c3ba9f40a140e3eb893e5c5d4ff923
Shaofuhw <sfmarauder@gmail.com>
2016-03-01 20:09:45 +0100

    03 - Limpieza
    Ordenado un poco el código, y separado en un header

--- main.c
+++ main.c
@@ -7,8 +7,11 @@
 
 #include <xc.h>
 #include<pic16f883.h>
+#include "Funciones.h"
 
 #define _XTAL_FREQ 8000000
+
+// <editor-fold defaultstate="collapsed" desc="CONFIG1 y CONFIG2">
 
 // CONFIG1
 #pragma config FOSC = INTRC_CLKOUT  // Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
@@ -26,67 +29,30 @@
 #pragma config BOR4V = BOR21V       // Brown-out Reset Selection bit (Brown-out Reset set to 2.1V)
 #pragma config WRT = OFF                // Flash Program Memory Self Write Enable bits (Write protection off)
 
+// </editor-fold>
+
 //Funciones
 void Inicializaciones(void);
+void Trigger(void);
+void DistLED(int);
 
 //Variables Globales
 int dist = 0;
 
+
+//Programa
 void main(void) {
     Inicializaciones();
     while(1)
     {
-        RB0 = 1;                                //Se activa el trigger
-        __delay_us(10);
-        RB0 = 0;                                //Se apaga el trigger tras retardo
+        Trigger();                              //Activa el Trigger, y lo apaga trás un retardo
         __delay_ms(60);                    //Retardo de 60ms entre lecturas, tiempo recomendado por fabricante
-        if(dist <= 400)
-        {
-            RA0 = 1;
-            if(dist <= 300)
-            {
-                RA1 = 1;
-                if(dist <= 200)
-                {
-                    RA2 = 1;
-                    if(dist <= 100)
-                    {
-                        RA3 = 1;
-                    }
-                }
-            }
-        }
+        DistLED(dist);                      //Enciende LEDs según la distancia
     }
     return;
 }
 
-//Se inician todos los registros necesarios para configurar el puerto B, las interrupciones, y el timer1
-void Inicializaciones(){
-    OSCCON = 0b01110101;            //Se inicializa el oscilador a 8MHz, la máxima.
-    
-    //Puerto A
-    TRISA = 0b11110000;                 //Puerto A del 0 a 3 como salida.
-    PORTA = 0b00000000;
-    ANSEL = 0b00000000;
-    
-    //Puerto B
-    TRISB = 0b11110000;                 //Puerto B. Pines del 0 al 3 como salida. Resto entrada
-    PORTB = 0b00000000;             //Inicializar a 0
-    ANSELH = 0b00000000;           //Establecer como puertos Digitales
-    
-    /*Timer 1 Como se requiere contar hasta tiempo de hasta aproximadamente 40ms se coge el timer de 16bits
-     * que a una frecuencia de 8MHz, es capaz de contar 32.726ms.
-     * Si ponemos el prescaler a 2, podrá contar el doble. Hasta 65.536ms*/
-    T1CON = 0b00010000;             //Prescaler a 1:2, apagado
-    
-    //Configuración de las interrupciones
-    INTCON = 0b00001000;            //Interrupciones por cambio de estado puerto B y Periféricos
-    IOCB = 0b11110000;
-    PIE1 = 0b00000001;                  //Interrupción por overflow del timer1
-    PIR1 = 0b00000000;                  //Limpia las banderas de los periféricos
-    GIE = 1;                                    //Bit Interrupción Global
-}
-
+//Vector de Servicio de Interrupción
 void interrupt echo()
 {
     if(RBIF == 1)                           //Comprobar flag del puerto B
